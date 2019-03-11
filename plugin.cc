3786b8e676ab55870665b667858b3d9ce7a2f700
Ueki Masaru <masaru.ueki@itage.co.jp>
2019-03-11 17:26:15 +0900

    impl StandalonePlugin

--- plugin.cc
+++ plugin.cc
@@ -1,5 +1,7 @@
 #include "plugin.hh"
 #include <gazebo/common/Events.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/Link.hh>
 
 namespace gazebo {
 
@@ -8,18 +10,32 @@ GZ_REGISTER_MODEL_PLUGIN(StandalonePlugin)
 StandalonePlugin::StandalonePlugin(){}
 StandalonePlugin::~StandalonePlugin(){}
 
-void StandalonePlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+void StandalonePlugin::Load(physics::ModelPtr model, sdf::ElementPtr _sdf)
 {
     gzmsg << "Load StandalonePlugin\n";
 
     update_conn_ = event::Events::ConnectWorldUpdateBegin(std::bind(&StandalonePlugin::OnUpdate, this));
+    model_ = model;
+
+    node_ = gazebo::transport::NodePtr(new gazebo::transport::Node());
+    node_->Init();
+
+    sub_ = node_->Subscribe("/velocity", &StandalonePlugin::CbMsgVelocity, this);
+
 }
 
 void StandalonePlugin::OnUpdate() {
-    gzmsg << "call OnUpdate\n";
+    model_->GetJoint("front_right")->SetVelocity(0, vel_);
+    model_->GetJoint("front_left")->SetVelocity(0, vel_);
+    model_->GetJoint("rear_right")->SetVelocity(0, vel_);
+    model_->GetJoint("rear_left")->SetVelocity(0, vel_);
 }
 
 void StandalonePlugin::Reset() {
+}
+
+void StandalonePlugin::CbMsgVelocity(MsgVelocityPtr& recv_vel) {
+    vel_ = recv_vel->vel();
 }
 
 }
--- plugin.hh
+++ plugin.hh
@@ -2,13 +2,28 @@
 
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/UpdateInfo.hh>
+#include <gazebo/transport/transport.hh>
+
+#include <boost/shared_ptr.hpp>
+
+#include <atomic>
+#include <cstdint>
+
+#include "velocity.pb.h"
 
 namespace gazebo
 {
 
+using MsgVelocityPtr = const boost::shared_ptr<const standalone_plugin::msgs::Velocity>;
+
 class GAZEBO_VISIBLE StandalonePlugin : public ModelPlugin
 {
     event::ConnectionPtr update_conn_;
+    physics::ModelPtr model_;
+    transport::NodePtr node_;
+    transport::SubscriberPtr sub_;
+
+    std::atomic<int32_t> vel_ = 0;
 public:
     StandalonePlugin();
     ~StandalonePlugin();
@@ -17,6 +32,8 @@ public:
     void Reset() override;
 
     void OnUpdate();
+
+    void CbMsgVelocity(MsgVelocityPtr& recv_vel);
 };
 
 }
